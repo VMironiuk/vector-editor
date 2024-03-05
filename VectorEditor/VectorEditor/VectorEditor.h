c7f1036fa7205b81c7e169c53c5dc9ec4231a5a0
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2024-03-05 12:52:13 +0200

    Initial project

---
+++ VectorEditor/VectorEditor/VectorEditor.h
@@ -0,0 +1,18 @@
+//
+//  VectorEditor.h
+//  VectorEditor
+//
+//  Created by Volodymyr Myroniuk on 05.03.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VectorEditor.
+FOUNDATION_EXPORT double VectorEditorVersionNumber;
+
+//! Project version string for VectorEditor.
+FOUNDATION_EXPORT const unsigned char VectorEditorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VectorEditor/PublicHeader.h>
+
+
---
+++ VectorEditor/VectorEditorTests/DocumentStoreTests.swift
@@ -0,0 +1,11 @@
+//
+//  DocumentStoreTests.swift
+//  VectorEditorTests
+//
+//  Created by Volodymyr Myroniuk on 05.03.2024.
+//
+
+import XCTest
+
+final class DocumentStoreTests: XCTestCase {
+}
