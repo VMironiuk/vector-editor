27c91f875ce3b359f836d7463263ac352a155af6
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2024-03-05 14:04:08 +0200

    Add DocumentManagerTests

--- VectorEditor/VectorEditorTests/DocumentManagerTests.swift
+++ VectorEditor/VectorEditorTests/DocumentManagerTests.swift
@@ -7,20 +7,81 @@
 
 import XCTest
 
-final class DocumentManager {}
+protocol DocumentStoreProtocol {
+    func load()
+    func save()
+}
+
+final class DocumentManager {
+    private let store: DocumentStoreProtocol
+    
+    init(store: DocumentStoreProtocol) {
+        self.store = store
+    }
+    
+    func load() {
+        store.load()
+    }
+    
+    func save() {
+        store.save()
+    }
+}
 
 final class DocumentManagerTests: XCTestCase {
-    func test_init_doesNotCallLoadOrSave() {
+    func test_init_doesNotLoadOrSave() {
         let store = DocumentStoreSpy()
-        let sut  = DocumentManager()
+        _ = DocumentManager(store: store)
         
         XCTAssertEqual(store.loadCallCount, 0)
         XCTAssertEqual(store.saveCallCount, 0)
     }
     
+    func test_load_loadsDocumentOnLoad() {
+        let store = DocumentStoreSpy()
+        let sut = DocumentManager(store: store)
+        
+        sut.load()
+        
+        XCTAssertEqual(store.loadCallCount, 1)
+        XCTAssertEqual(store.saveCallCount, 0)
+    }
+    
+    func test_load_loadsDocumentTwiceOnCallingLoadTwice() {
+        let store = DocumentStoreSpy()
+        let sut = DocumentManager(store: store)
+        
+        sut.load()
+        sut.load()
+        
+        XCTAssertEqual(store.loadCallCount, 2)
+        XCTAssertEqual(store.saveCallCount, 0)
+    }
+    
+    func test_load_savesDocumentOnSave() {
+        let store = DocumentStoreSpy()
+        let sut = DocumentManager(store: store)
+        
+        sut.save()
+        
+        XCTAssertEqual(store.loadCallCount, 0)
+        XCTAssertEqual(store.saveCallCount, 1)
+    }
+    
+    func test_load_savesDocumentTwiceOnCallingSaveTwice() {
+        let store = DocumentStoreSpy()
+        let sut = DocumentManager(store: store)
+        
+        sut.save()
+        sut.save()
+        
+        XCTAssertEqual(store.loadCallCount, 0)
+        XCTAssertEqual(store.saveCallCount, 2)
+    }
+    
     // MARK: - Helpers
     
-    final class DocumentStoreSpy {
+    private final class DocumentStoreSpy: DocumentStoreProtocol {
         private(set) var loadCallCount = 0
         private(set) var saveCallCount = 0
         
