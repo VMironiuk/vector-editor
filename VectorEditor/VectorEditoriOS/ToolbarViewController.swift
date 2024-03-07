d93709d958f540a7f840d9acd3c3cd602baeeeb7
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2024-03-07 13:05:19 +0200

    Add ToolbarView

---
+++ VectorEditor/VectorEditoriOS/ToolbarViewController.swift
@@ -0,0 +1,11 @@
+//
+//  ToolbarViewController.swift
+//  VectorEditoriOS
+//
+//  Created by Volodymyr Myroniuk on 07.03.2024.
+//
+
+import UIKit
+
+public final class ToolbarViewController: UIViewController {
+}
---
+++ VectorEditor/VectorEditoriOS/VectorEditoriOS.h
@@ -0,0 +1,18 @@
+//
+//  VectorEditoriOS.h
+//  VectorEditoriOS
+//
+//  Created by Volodymyr Myroniuk on 07.03.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VectorEditoriOS.
+FOUNDATION_EXPORT double VectorEditoriOSVersionNumber;
+
+//! Project version string for VectorEditoriOS.
+FOUNDATION_EXPORT const unsigned char VectorEditoriOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VectorEditoriOS/PublicHeader.h>
+
+
