721e1b080d4b180f6252edf215df1c31d43ab488
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2024-03-07 11:07:20 +0200

    ToolbarViewModel.init sets supported shapes

--- VectorEditor/VectorEditorTests/ToolbarViewModelTests.swift
+++ VectorEditor/VectorEditorTests/ToolbarViewModelTests.swift
@@ -7,19 +7,35 @@
 
 import XCTest
 
+enum SupportedShape: CaseIterable {
+    case circle
+    case rectangle
+}
+
 final class ToolbarViewModel {
     let documentName: String
+    let supportedShapes: [SupportedShape]
     
-    init(documentName: String) {
+    init(documentName: String, supportedShapes: [SupportedShape]) {
         self.documentName = documentName
+        self.supportedShapes = supportedShapes
     }
 }
 
 final class ToolbarViewModelTests: XCTestCase {
     func test_init_setsDocumentName() {
         let documentName = "document name"
-        let sut = ToolbarViewModel(documentName: documentName)
+        let sut = ToolbarViewModel(documentName: documentName, supportedShapes: SupportedShape.allCases)
         
         XCTAssertEqual(documentName, sut.documentName)
     }
+    
+    func test_init_setsSupportedShapes() {
+        let supportedShapes = SupportedShape.allCases
+        let sut = ToolbarViewModel(documentName: "", supportedShapes: supportedShapes)
+        
+        XCTAssertEqual(supportedShapes, sut.supportedShapes)
+    }
 }
+
+extension SupportedShape: Equatable {}
