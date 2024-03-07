25d53179d5b13f9e399dac8227ca08351425b5ee
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2024-03-07 11:15:23 +0200

    ToolbarViewModel.init does not inform its delegate about selected shape

--- VectorEditor/VectorEditorTests/ToolbarViewModelTests.swift
+++ VectorEditor/VectorEditorTests/ToolbarViewModelTests.swift
@@ -12,9 +12,15 @@ enum SupportedShape: CaseIterable {
     case rectangle
 }
 
+protocol ToolbarViewModelDelegate: AnyObject {
+    func didSelectShape(_ shape: SupportedShape)
+}
+
 final class ToolbarViewModel {
     let documentName: String
     let supportedShapes: [SupportedShape]
+    
+    weak var delegate: ToolbarViewModelDelegate?
     
     init(documentName: String, supportedShapes: [SupportedShape]) {
         self.documentName = documentName
@@ -35,6 +41,24 @@ final class ToolbarViewModelTests: XCTestCase {
         let sut = ToolbarViewModel(documentName: "", supportedShapes: supportedShapes)
         
         XCTAssertEqual(supportedShapes, sut.supportedShapes)
+    }
+    
+    func test_init_doesNotInformDelegateAboutSelectedShape() {
+        let delegate = ToolbarViewModelDelegateSpy()
+        let sut = ToolbarViewModel(documentName: "", supportedShapes: SupportedShape.allCases)
+        sut.delegate = delegate
+        
+        XCTAssertEqual(delegate.didSelectShapeCallCount, 0)
+    }
+    
+    // MARK: - Helpers
+    
+    private class ToolbarViewModelDelegateSpy: ToolbarViewModelDelegate {
+        private(set) var didSelectShapeCallCount = 0
+        
+        func didSelectShape(_ shape: SupportedShape) {
+            didSelectShapeCallCount += 1
+        }
     }
 }
 
